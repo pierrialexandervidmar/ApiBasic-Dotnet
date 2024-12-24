26c0c327e22d6e0c8edd81dd30c82ca3203f74b1
pierrialexandervidmar <pierri@hotmail.com.br>
2024-12-24 18:13:09 -0300

    Definido rota Get para buscar todos

--- Estudantes/EstudantesRotas.cs
+++ Estudantes/EstudantesRotas.cs
@@ -1,4 +1,5 @@
 using ApiBasic.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiBasic.Estudantes;
 
@@ -10,10 +11,46 @@ public static class EstudantesRotas
         
         // POST PARA CRIAR
         rotasEstudantes.MapPost("", async (AddEstudanteRequest request, AppDbContext context) =>
-        {
-            var novoEstudante = new Estudante(request.Nome);
-            await context.Estudantes.AddAsync(novoEstudante);
-            await context.SaveChangesAsync();
-        });
+            {
+                var jaExiste = await context.Estudantes
+                    .AnyAsync(estudante => estudante.Nome == request.Nome);
+                
+                if (jaExiste)
+                    return Results.Conflict("Usuário já existe na base de dados");
+                
+                try
+                {
+                    var novoEstudante = new Estudante(request.Nome);
+                    await context.Estudantes.AddAsync(novoEstudante);
+                    await context.SaveChangesAsync();
+
+                    // Retorna 201 Created com o estudante recém-criado                              
+                    return Results.Created($"/estudantes/{novoEstudante.Id}", novoEstudante);
+                }
+                catch (Exception ex)
+                {
+                    // Em caso de erro, retorna 400 Bad Request com uma mensagem
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+            })
+            .WithName("CriarEstudante")
+            .WithTags("Estudantes")
+            .WithSummary("Rota responsável por criar um novo estudante.")
+            .Produces<Estudante>(StatusCodes.Status201Created) // Indica o tipo de resposta e código HTTP
+            .Produces(StatusCodes.Status400BadRequest);
+        
+        // GET PARA RETORNAR TODOS OS USUÁRIOS
+        rotasEstudantes.MapGet("", async (AppDbContext context) =>
+            {
+                var estudantes = await context
+                    .Estudantes
+                    .Where(estudante => estudante.Ativo)
+                    .ToListAsync();
+
+                return Results.Ok(estudantes);
+            })
+            .WithTags("Estudantes")
+            .WithSummary("Rota responsável por buscar todos os estudantes.")
+            .Produces<List<Estudante>>(StatusCodes.Status200OK); // Indica o tipo de resposta e código HTTP
     }
 }
--- obj/Debug/net8.0/ApiBasic.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiBasic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a646c2026724fbb27aa3c48a76cbaeaaaee8be75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3054deec35a50167f248b1af6e542709689dc5ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiBasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
